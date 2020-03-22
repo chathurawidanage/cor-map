28a6266be30a8a8ca8c1e5cc9f9f9de15d17a712
Austin McGee <947888+amcgee@users.noreply.github.com>
2020-03-22 18:03:08 +0100

    fix: rectify bugs introduced by platform port

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useDataEngine } from '@dhis2/app-runtime'
 import App from './components/App'
 
+import './locales/index'
+
 const ContactTracingApp = () => {
     const engine = useDataEngine()
 
--- src/components/TEIDrawer.js
+++ src/components/TEIDrawer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useDataQuery, useConfig } from '@dhis2/app-runtime';
+import i18n from '@dhis2/d2-i18n'
 import {Button, Drawer, Tag} from "@blueprintjs/core";
 import Loader from "./loader/Loader";
 import "./TEIDrawer.css";
@@ -7,12 +8,18 @@ import * as DHIS2Constants from "../DHIS2Constants";
 import {Intent} from "@blueprintjs/core/lib/cjs/common/intent";
 import { trackedEntityInstanceQuery } from "../queries/TEIQueries";
 
+import "@blueprintjs/core/lib/css/blueprint.css";
+
 const TEIDrawer = ({ te, onClose }) => {
     const { baseUrl } = useConfig()
-    const { loading, error, data } = useDataQuery(trackedEntityInstanceQuery)
+    const { loading, error, data } = useDataQuery(trackedEntityInstanceQuery, { 
+        variables: {
+            id: te.id
+        }
+    })
 
     const openInTracker = () => {
-        let url = `${baseUrl}/dhis-web-tracker-capture/index.html#/dashboard?tei=${te.id}&program=${te.program}&ou=${data?.tei.orgUnit}`;
+        let url = `${baseUrl}dhis-web-tracker-capture/index.html#/dashboard?tei=${te.id}&program=${te.program}&ou=${data?.tei.orgUnit}`;
         window.open(url, "_blank");
     };
 
@@ -24,6 +31,8 @@ const TEIDrawer = ({ te, onClose }) => {
             </div>
         );
     });
+    
+    console.log(trackedEntityInstanceQuery, error)
 
     return (
         <div>
