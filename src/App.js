f0e18fe8198ccd150eb9d0c3c6b48651a150d2c2
Chathura Widanage <chathurawidanage@gmail.com>
2020-03-22 01:17:18 -0400

    minor bug fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useDataEngine } from '@dhis2/app-runtime'
-import App from './components/App'
+import App from './components/App';
+import "@blueprintjs/core/lib/css/blueprint.css"
 
 const ContactTracingApp = () => {
-    const engine = useDataEngine()
+    const engine = useDataEngine();
 
     return <App engine={engine} />
-}
+};
 
 export default ContactTracingApp
