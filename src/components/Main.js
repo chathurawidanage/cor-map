9fece4685b4cd2fffc5967a28a0a478cb895f13b
Chathura Widanage <chathurawidanage@gmail.com>
2020-04-13 21:13:48 -0400

    Changed element order

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 import {Button} from "@dhis2/ui-core";
 import "./Main.css";
-import { VisualizationList } from "./VisualizationList";
+import {VisualizationList} from "./VisualizationList";
 
 export default class Main extends React.Component {
 
@@ -10,10 +10,10 @@ export default class Main extends React.Component {
         return (
             <div className="main-wrapper">
                 <h3>Visualizations</h3>
-                <VisualizationList />
                 <Link to="new">
                     <Button primary={true}>Create</Button>
                 </Link>
+                <VisualizationList/>
             </div>
         );
     }
